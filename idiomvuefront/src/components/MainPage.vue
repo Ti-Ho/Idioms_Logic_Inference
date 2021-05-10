a3a9bcb4ae34b4290a52eedf90ba54ee4a1d3419
zth <380041632@qq.com>
2021-05-10 11:12:30 +0800

    frontend

--- idiomvuefront/src/components/MainPage.vue
+++ idiomvuefront/src/components/MainPage.vue
@@ -3,34 +3,16 @@
     <el-container>
       <el-header>汉语成语逻辑关系推断可视化面板</el-header>
       <el-main>
-<!--        <el-row>*步骤1: 输入成语</el-row>-->
-<!--        <el-row>-->
-<!--          <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>-->
-<!--          <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>-->
-<!--        </el-row>-->
-<!--        <el-divider></el-divider>-->
-<!--        <el-row>*步骤2: 选择模型</el-row>-->
-<!--        <el-row>-->
-<!--          <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>-->
-<!--          <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>-->
-<!--        </el-row>-->
-<!--        <el-divider></el-divider>-->
-<!--        <el-row>-->
-<!--          <el-col :span="24"><div class="grid-content bg-purple-dark">步骤3：开始推断</div></el-col>-->
-<!--        </el-row>-->
-<!--        <el-divider></el-divider>-->
-<!--        <el-row>-->
-<!--          图表-->
-<!--        </el-row>-->
-
-        <el-form ref="form" :model="form" label-width="80px">
-          <el-row>*步骤1: 输入成语</el-row>
+        <!--     form 表单      -->
+        <el-form ref="form" :model="form" label-width="120px" label-position="left">
+          <el-row><div class="step-font">步骤1: 输入成语</div></el-row>
           <el-row>
             <el-col :span="7">
               <el-form-item label="成语1">
                 <el-input v-model="form.idiom1"></el-input>
               </el-form-item>
             </el-col>
+            <el-col :span="2"><div class="grid-content"></div></el-col>
             <el-col :span="7">
               <el-form-item label="成语2">
                 <el-input v-model="form.idiom2"></el-input>
@@ -38,51 +20,44 @@
             </el-col>
           </el-row>
           <el-divider></el-divider>
-          <el-row>*步骤2: 选择模型</el-row>
-
-          <el-form-item label="活动名称">
-            <el-input v-model="form.name"></el-input>
-          </el-form-item>
-          <el-form-item label="活动区域">
-            <el-select v-model="form.region" placeholder="请选择活动区域">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="活动时间">
-            <el-col :span="11">
-              <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+          <el-row><div class="step-font">步骤2: 选择推断模型</div></el-row>
+          <el-row>
+            <el-col :span="7">
+              <el-form-item label="选择模型">
+                <el-radio-group v-model="form.model_type">
+                  <el-radio label="二分类模型"></el-radio>
+                  <el-radio label="多分类模型"></el-radio>
+                </el-radio-group>
+              </el-form-item>
             </el-col>
-            <el-col class="line" :span="2">-</el-col>
-            <el-col :span="11">
-              <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+            <el-col :span="2"><div class="grid-content"></div></el-col>
+            <el-col :span="10">
+              <el-form-item label="选择输出层结构">
+                <el-radio-group v-model="form.ifPool">
+                  <el-radio label="使用mean max pool输出层"></el-radio>
+                  <el-radio label="使用CLS向量推断"></el-radio>
+                </el-radio-group>
+              </el-form-item>
             </el-col>
-          </el-form-item>
-          <el-form-item label="即时配送">
-            <el-switch v-model="form.delivery"></el-switch>
-          </el-form-item>
-          <el-form-item label="活动性质">
-            <el-checkbox-group v-model="form.type">
-              <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-              <el-checkbox label="地推活动" name="type"></el-checkbox>
-              <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-              <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-            </el-checkbox-group>
-          </el-form-item>
-          <el-form-item label="特殊资源">
-            <el-radio-group v-model="form.resource">
-              <el-radio label="线上品牌商赞助"></el-radio>
-              <el-radio label="线下场地免费"></el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item label="活动形式">
-            <el-input type="textarea" v-model="form.desc"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-            <el-button>取消</el-button>
-          </el-form-item>
+          </el-row>
+          <el-divider></el-divider>
+          <el-row>
+            <el-col :span=7>
+              <div class="step-font">步骤3: 开始推断</div>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item>
+                <el-button type="primary" @click="onSubmit">开始推断</el-button>
+                <el-button @click="clear">取消</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
         </el-form>
+        <el-divider></el-divider>
+        <!--     结果图表      -->
+        <el-row style="height: 55%">
+          <div class="step-font">模型推断结果</div>
+        </el-row>
       </el-main>
     </el-container>
   </div>
@@ -94,16 +69,10 @@ export default {
   data() {
     return {
       form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: '',
         idiom1: '',
-        idiom2: ''
+        idiom2: '',
+        model_type: '二分类模型',
+        ifPool: '使用mean max pool输出层'
       }
     }
   },
@@ -111,6 +80,10 @@ export default {
     onSubmit() {
       console.log('submit!');
       console.log(this.form);
+    },
+    clear() {
+      this.form.idiom1 = '';
+      this.form.idiom2 = '';
     }
   }
 }
@@ -120,7 +93,7 @@ export default {
 .screen-container {
     width: 100%;
     height: 100%;
-    padding: 0 50px;
+    padding: 0 100px;
     /*background-color: #161522;*/
     /*color: #fff;*/
     box-sizing: border-box;
@@ -140,7 +113,7 @@ export default {
 }
 
 .el-row {
-  margin-bottom: 10px;
+  margin-bottom: 5px;
 }
 .el-col {
   border-radius: 4px;
@@ -160,9 +133,16 @@ export default {
 }
 
 .el-divider {
-  height: 2px;
+  height: 1px;
   /*background-color: #003366;*/
   margin-top: 0px !important;
-  margin-bottom: 0px !important;
+  margin-bottom: 10px !important;
+}
+.step-font{
+  font-weight: bold;
+  font-family: 'Microsoft YaHei';
+  margin-left: 0px;
+  font-size: medium;
+  color: dimgrey;
 }
 </style>
