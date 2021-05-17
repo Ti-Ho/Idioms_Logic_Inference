4c6abc43b2b6f4334341207628eb90c54d84fa2e
zth <380041632@qq.com>
2021-05-17 21:43:20 +0800

    complete model compare component and add training_log

--- idiomvuefront/src/components/modelCompareComponent.vue
+++ idiomvuefront/src/components/modelCompareComponent.vue
@@ -1,50 +1,5 @@
 <template>
   <div style="margin-left: 10px; margin-right: 10px; height: 100%">
-    <el-form ref="form" :model="comForm" label-width="120px" label-position="left">
-      <el-row>
-        <el-col :span="12">
-          <el-row><div class="step-font">选择对比推断模型1</div></el-row>
-          <el-row>
-              <el-form-item label="选择模型">
-                <el-radio-group v-model="comForm.model_type1" @change="modelChange">
-                  <el-radio label="二分类模型" border></el-radio>
-                  <el-radio label="多分类模型" border></el-radio>
-                </el-radio-group>
-              </el-form-item>
-          </el-row>
-        </el-col>
-        <el-col :span="12">
-          <el-row><div class="step-font">选择对比推断模型2</div></el-row>
-          <el-row>
-            <el-form-item label="选择模型">
-              <el-radio-group v-model="comForm.model_type2" @change="modelChange">
-                <el-radio label="二分类模型" border></el-radio>
-                <el-radio label="多分类模型" border></el-radio>
-              </el-radio-group>
-            </el-form-item>
-          </el-row>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="12">
-          <el-form-item label="选择输出层结构">
-            <el-radio-group v-model="comForm.ifPool1" @change="modelChange">
-              <el-radio label="使用mean max pool输出层" border></el-radio>
-              <el-radio label="使用CLS向量推断" border></el-radio>
-            </el-radio-group>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="选择输出层结构">
-            <el-radio-group v-model="comForm.ifPool2" @change="modelChange">
-              <el-radio label="使用mean max pool输出层" border></el-radio>
-              <el-radio label="使用CLS向量推断" border></el-radio>
-            </el-radio-group>
-          </el-form-item>
-        </el-col>
-      </el-row>
-    </el-form>
-    <el-divider></el-divider>
     <div class="line-div">
       <div class="com-chart" ref="lossLineChart_ref"></div>
     </div>
@@ -63,29 +18,80 @@ export default {
         ifPool2: '使用mean max pool输出层',
       },
       chartInstance: null,
-      linedata: {
+      lineData: {
         bi_cls_1: {
-          train_loss: [],
+          train_loss: [0.69103194, 0.68238538, 0.68032131, 0.67901261, 0.67758783,
+            0.67650106, 0.6751258 , 0.6738251 , 0.67256694, 0.67028133,
+            0.66863984, 0.6660909 , 0.66466657, 0.66267761, 0.66123487,
+            0.66019796, 0.65880101, 0.65775274, 0.65619658, 0.6551392 ,
+            0.65423207, 0.65288923, 0.6521953 , 0.65108405, 0.65080682,
+            0.64928919, 0.64873894, 0.64762353, 0.64663586, 0.64584352,
+            0.64503548, 0.64463794, 0.64343582, 0.64248558, 0.64220043,
+            0.64033673, 0.63996848, 0.63887943, 0.63760106, 0.63749026,
+            0.63614766, 0.63549968, 0.63559033, 0.63441819, 0.63287746,
+            0.63350336, 0.63247115, 0.63098343, 0.63048146, 0.63005171,
+            0.62814139, 0.62891789, 0.62884938, 0.62740247, 0.62657902,
+            0.62746486],
           xAxis_epoch: []
         },
         bi_cls_2: {
-          train_loss: [],
+          train_loss: [0.51527147, 0.49429313, 0.48266765, 0.47211093, 0.46515002,
+            0.45984472, 0.45549583, 0.45211305, 0.44783983, 0.4450493 ,
+            0.44295325, 0.44074994, 0.43557786, 0.43468031, 0.43225713,
+            0.43032336, 0.42814477, 0.42540171, 0.42482605, 0.42181086,
+            0.42144039, 0.42016819, 0.41790145, 0.41624597, 0.41447649,
+            0.4146623 , 0.41316424, 0.41136266, 0.41113708, 0.41124574,
+            0.40978776, 0.40950291, 0.4096256 ],
           xAxis_epoch: []
         },
         bi_pool_1: {
-          train_loss: [],
+          train_loss: [0.6804877 , 0.67429761, 0.66823514, 0.66401107, 0.65899839,
+            0.65336343, 0.6485853 , 0.6431668 , 0.63824807, 0.63300982,
+            0.62861104, 0.62325678, 0.61850822, 0.61548305, 0.61158493,
+            0.60517312, 0.60356534, 0.60005886, 0.59427461, 0.59521495,
+            0.58953779, 0.58659312, 0.5837733 , 0.58210126, 0.5792641 ,
+            0.57678444, 0.57599075, 0.57314692, 0.56986898, 0.56783211,
+            0.56693238, 0.56746361, 0.56396085, 0.56465711, 0.56384179,
+            0.56236042, 0.55888086, 0.55896783, 0.5591759 , 0.5605095 ],
           xAxis_epoch: []
         },
         bi_pool_2: {
-          train_loss: [],
+          train_loss: [0.50223166, 0.4875405 , 0.47875468, 0.47207307, 0.46688411,
+            0.46251392, 0.45790269, 0.45394192, 0.44974721, 0.44794315,
+            0.44381461, 0.44264354, 0.43897576, 0.43728126, 0.43583241,
+            0.43390693, 0.43208422, 0.42942715, 0.42943285, 0.42731233,
+            0.42506186, 0.42465089, 0.42271115, 0.42243075, 0.42178289,
+            0.41845532, 0.41906372, 0.41729086, 0.41712185, 0.4158252 ,
+            0.41589313, 0.41431159, 0.41616754, 0.4139873 ],
           xAxis_epoch: []
         },
         multi_cls: {
-          train_loss: [],
+          train_loss: [1.09024125, 1.0623899 , 1.0378297 , 1.01855199, 1.00708282,
+            0.99995562, 0.99563378, 0.99311443, 0.99034541, 0.98837346,
+            0.98703842, 0.98559544, 0.98340604, 0.98161594, 0.98147206,
+            0.97889631, 0.97892533, 0.97664821, 0.97536475, 0.97452495,
+            0.97346063, 0.9730385 , 0.97167261, 0.96967415, 0.97014967,
+            0.96956534, 0.96894987, 0.96788465, 0.96782416, 0.96682636,
+            0.9661805 , 0.96613373, 0.96664346, 0.96523445, 0.96462047,
+            0.96592017, 0.96464219, 0.96363319, 0.96351703, 0.96245498,
+            0.96293088, 0.96266475, 0.96273651, 0.96167047, 0.96267364,
+            0.96163786, 0.96102894, 0.961822  , 0.9617052 , 0.96191596,
+            0.96162592],
           xAxis_epoch: []
         },
         multi_pool: {
-          train_loss: [],
+          train_loss: [1.0373688 , 0.99954391, 0.99157605, 0.98784667, 0.98525307,
+            0.98253595, 0.9795602 , 0.97716979, 0.97400375, 0.9717297 ,
+            0.96852497, 0.96594896, 0.96277808, 0.96013765, 0.95749585,
+            0.95400866, 0.95047928, 0.94869281, 0.94654232, 0.94384885,
+            0.94037837, 0.9404654 , 0.93891471, 0.93619817, 0.93507252,
+            0.9339158 , 0.93241709, 0.93136947, 0.93092031, 0.92868649,
+            0.92832957, 0.92697858, 0.9269564 , 0.92483477, 0.92520076,
+            0.92430654, 0.92382296, 0.92351595, 0.92368185, 0.92206069,
+            0.92183608, 0.92243669, 0.92140265, 0.92124816, 0.919753  ,
+            0.9197892 , 0.91907641, 0.91991825, 0.91937882, 0.91795984,
+            0.9183317 , 0.91937876, 0.91783003, 0.91867194, 0.91816645,
+            0.91813608, 0.91858871],
           xAxis_epoch: []
         }
       }
@@ -93,7 +99,6 @@ export default {
   },
   mounted () {
     this.initChart();
-    console.log("model compare Component mounted")
   },
   methods : {
     // 初始化echartsInstance对象
@@ -101,18 +106,21 @@ export default {
       this.chartInstance = this.$echarts.init(this.$refs.lossLineChart_ref);
       const initOption = {
         title: {
+          top: '5%',
           text: '模型对比'
         },
         tooltip: {
           trigger: 'axis'
         },
         legend: {
-          data: ['二分类#CLS向量#并列关系', '二分类#CLS向量#转折关系', '二分类#MMP#并列关系', '二分类#MMP#转折关系', '多分类#CLS', '多分类#MMP']
+          // data: ['二分类#CLS向量#并列关系', '二分类#CLS向量#转折关系', '二分类#MMP#并列关系', '二分类#MMP#转折关系', '多分类#CLS', '多分类#MMP']
+          top: '5%'
         },
         grid: {
-          left: '3%',
+          left: '1%',
           right: '4%',
           bottom: '3%',
+          top: '13%',
           containLabel: true
         },
         toolbox: {
@@ -123,120 +131,53 @@ export default {
         xAxis: {
           type: 'category',
           boundaryGap: false,
-          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          data: ['epoch0', 'epoch1', 'epoch2', 'epoch3', 'epoch4', 'epoch5', 'epoch6', 'epoch7',
+            'epoch8','epoch9','epoch10','epoch11','epoch12','epoch13','epoch14',
+            'epoch15','epoch16','epoch17','epoch18','epoch19','epoch20','epoch21',
+            'epoch22','epoch23','epoch24','epoch25','epoch26','epoch27','epoch28',
+            'epoch29','epoch30','epoch31','epoch32','epoch33','epoch34','epoch35',
+            'epoch36','epoch37','epoch38','epoch39','epoch40','epoch41','epoch42',
+            'epoch43','epoch44','epoch45','epoch46','epoch47','epoch48','epoch49',
+            'epoch50','epoch51','epoch52','epoch53','epoch54','epoch55','epoch56']
         },
         yAxis: {
           type: 'value'
         },
         series: [
           {
-            name: '二分类#CLS向量#转折关系',
+            name: '二分类#CLS#并列关系',
             type: 'line',
-            data: [220, 182, 191, 234, 290, 330, 310]
+            data: this.lineData.bi_cls_1.train_loss
+          },
+          {
+            name: '二分类#CLS#转折关系',
+            type: 'line',
+            data: this.lineData.bi_cls_2.train_loss
           },
           {
             name: '二分类#MMP#并列关系',
             type: 'line',
-            data: [150, 232, 201, 154, 190, 330, 410]
+            data: this.lineData.bi_pool_1.train_loss
           },
           {
             name: '二分类#MMP#转折关系',
             type: 'line',
-            data: [320, 332, 301, 334, 390, 330, 320]
+            data: this.lineData.bi_pool_2.train_loss
           },
           {
             name: '多分类#CLS',
             type: 'line',
-            data: [820, 932, 901, 934, 1290, 1330, 1320]
+            data: this.lineData.multi_cls.train_loss
           },
           {
             name: '多分类#MMP',
             type: 'line',
-            data: [120, 240, 500, 780, 700, 490, 982]
+            data: this.lineData.multi_pool.train_loss
           }
         ]
       };
       this.chartInstance.setOption(initOption);
     },
-    modelChange () {
-      if (this.comForm.model_type1 === '二分类模型' && this.comForm.ifPool1 === "使用mean max pool输出层"
-          && this.comForm.model_type1 === "二分类模型" && this.comForm.ifPool2 === "使用mean max pool输出层"){
-        const dataOption = {
-          series: [
-            {
-              name: '二分类#MMP#转折关系',
-              type: 'line',
-              data: [320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-              name: '二分类#MMP#并列关系',
-              type: 'line',
-              data: [150, 232, 201, 154, 190, 330, 410]
-            },
-          ]
-        };
-        this.chartInstance.setOption(dataOption);
-        console.log(1)
-      }
-      else if (this.comForm.model_type1 === '二分类模型' && this.comForm.ifPool1 === "使用mean max pool输出层"
-              && this.comForm.model_type1 === "二分类模型" && this.comForm.ifPool2 !== "使用mean max pool输出层"){
-        console.log(2)
-      }
-      else if (this.comForm.model_type1 === '二分类模型' && this.comForm.ifPool1 === "使用mean max pool输出层"
-              && this.comForm.model_type1 !== "二分类模型" && this.comForm.ifPool2 === "使用mean max pool输出层"){
-        console.log(3)
-      }
-      else if (this.comForm.model_type1 === '二分类模型' && this.comForm.ifPool1 === "使用mean max pool输出层"
-              && this.comForm.model_type1 !== "二分类模型" && this.comForm.ifPool2 !== "使用mean max pool输出层"){
-        console.log(4)
-      }
-      else if (this.comForm.model_type1 === '二分类模型' && this.comForm.ifPool1 !== "使用mean max pool输出层"
-              && this.comForm.model_type1 === "二分类模型" && this.comForm.ifPool2 === "使用mean max pool输出层"){
-        console.log(5)
-      }
-      else if (this.comForm.model_type1 === '二分类模型' && this.comForm.ifPool1 !== "使用mean max pool输出层"
-              && this.comForm.model_type1 === "二分类模型" && this.comForm.ifPool2 !== "使用mean max pool输出层"){
-        console.log(6)
-      }
-      else if (this.comForm.model_type1 === '二分类模型' && this.comForm.ifPool1 !== "使用mean max pool输出层"
-              && this.comForm.model_type1 !== "二分类模型" && this.comForm.ifPool2 === "使用mean max pool输出层"){
-        console.log(7)
-      }
-      else if (this.comForm.model_type1 === '二分类模型' && this.comForm.ifPool1 !== "使用mean max pool输出层"
-              && this.comForm.model_type1 !== "二分类模型" && this.comForm.ifPool2 !== "使用mean max pool输出层"){
-        console.log(8)
-      }
-      else if (this.comForm.model_type1 !== '二分类模型' && this.comForm.ifPool1 === "使用mean max pool输出层"
-              && this.comForm.model_type1 === "二分类模型" && this.comForm.ifPool2 === "使用mean max pool输出层"){
-        console.log(9)
-      }
-      else if (this.comForm.model_type1 !== '二分类模型' && this.comForm.ifPool1 === "使用mean max pool输出层"
-              && this.comForm.model_type1 === "二分类模型" && this.comForm.ifPool2 !== "使用mean max pool输出层"){
-        console.log(10)
-      }
-      else if (this.comForm.model_type1 !== '二分类模型' && this.comForm.ifPool1 === "使用mean max pool输出层"
-              && this.comForm.model_type1 !== "二分类模型" && this.comForm.ifPool2 === "使用mean max pool输出层"){
-        console.log(11)
-      }
-      else if (this.comForm.model_type1 !== '二分类模型' && this.comForm.ifPool1 === "使用mean max pool输出层"
-              && this.comForm.model_type1 !== "二分类模型" && this.comForm.ifPool2 !== "使用mean max pool输出层"){
-        console.log(12)
-      }
-      else if (this.comForm.model_type1 !== '二分类模型' && this.comForm.ifPool1 !== "使用mean max pool输出层"
-              && this.comForm.model_type1 === "二分类模型" && this.comForm.ifPool2 === "使用mean max pool输出层"){
-        console.log(13)
-      }
-      else if (this.comForm.model_type1 !== '二分类模型' && this.comForm.ifPool1 !== "使用mean max pool输出层"
-              && this.comForm.model_type1 === "二分类模型" && this.comForm.ifPool2 !== "使用mean max pool输出层"){
-        console.log(14)
-      }
-      else if (this.comForm.model_type1 !== '二分类模型' && this.comForm.ifPool1 !== "使用mean max pool输出层"
-              && this.comForm.model_type1 !== "二分类模型" && this.comForm.ifPool2 === "使用mean max pool输出层"){
-        console.log(15)
-      }else {
-        console.log(16)
-      }
-    }
   }
 }
 </script>
@@ -261,7 +202,7 @@ export default {
   margin-bottom: 10px !important;
 }
 .line-div {
-  height: 68%;
+  height: 90%;
   width: 100%;
   text-align: center;
 }
