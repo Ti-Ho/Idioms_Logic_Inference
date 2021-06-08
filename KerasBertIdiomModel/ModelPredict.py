3c5a3990714d4e8136f03b88e9f52c53fb3d8be7
zth <380041632@qq.com>
2021-06-08 22:50:23 +0800

    add input for keras bert model predict

--- KerasBertIdiomModel/ModelPredict.py
+++ KerasBertIdiomModel/ModelPredict.py
@@ -6,20 +6,18 @@
 # @Software: PyCharm
 # 模型预测脚本
 
-import time
 import json
-import numpy as np
 import re
 from multiClsModelTrain import token_dict, OurTokenizer
 from keras.models import load_model
 from keras_bert import get_custom_objects
-import os
+
 maxlen = 300
 
 # 加载训练好的模型
-ifPool = 0      # 控制加载模型 1 - mean max pool; 0 - CLS
+ifPool = 1  # 控制加载模型 1 - mean max pool; 0 - CLS
 syn_or_ant = 0  # 控制加载并列关系还是转折关系模型 0 - 并列关系; 0 - 转折关系
-model_type = 1  # 控制加载模型为 0 - 多分类 1 - 二分类
+model_type = 0  # 控制加载模型为 0 - 多分类 1 - 二分类
 
 if model_type == 0:
     if ifPool == 0:
@@ -46,6 +44,7 @@ else:
 
 tokenizer = OurTokenizer(token_dict)
 
+
 # 获取两个成语的解释和举例
 def getExplanationAndExample(idiom1, idiom2, idiomDict):
     print(idiom1 + " " + idiom2)
@@ -68,9 +67,6 @@ def getExplanationAndExample(idiom1, idiom2, idiomDict):
     explanation2 = dic2['explanation']
     return explanation1, example1, explanation2, example2
 
-s_time = time.time()
-# 预测示例成语
-InputIdiom = ["春暖花开", "春意盎然"]
 
 idiomDict = {}
 # 读取idiom.json中的数据 重构f
@@ -83,24 +79,43 @@ with open('corpus/idiom.json', 'r', encoding="utf-8") as f:
         idiomDict[word]["explanation"] = data["explanation"]
         idiomDict[word]["example"] = data["example"]
 
-# 获得成语的解释 举例
-explanation1, example1, explanation2, example2 = getExplanationAndExample(InputIdiom[0], InputIdiom[1], idiomDict)
-if explanation1 == None:
-    print(InputIdiom[0] + "不是成语")
-    os._exit()
-if explanation2 == None:
-    print(InputIdiom[1] + "不是成语")
-    os._exit()
+while True:
+    idiom1 = input("请输入第一个成语：")
+    idiom2 = input("请输入第二个成语：")
+    # 预测示例成语
+    InputIdiom = [idiom1, idiom2]
+    flag = 0
+    # 获得成语的解释 举例
+    explanation1, example1, explanation2, example2 = getExplanationAndExample(InputIdiom[0], InputIdiom[1], idiomDict)
+    if explanation1 == None:
+        print(InputIdiom[0] + "不是成语")
+        flag = 1
+    if explanation2 == None:
+        print(InputIdiom[1] + "不是成语")
+        flag = 1
 
-text1 = explanation1 if example1 == "无" else explanation1 + example1
-text2 = explanation2 if example2 == "无" else explanation2 + example2
-text1 = text1[: maxlen//2]
-text2 = text2[: maxlen//2]
-x1, x2 = tokenizer.encode(first=text1, second=text2)
+    if flag == 1:
+        continue
 
-X1 = x1 + [0] * (maxlen-len(x1)) if len(x1) < maxlen else x1
-X2 = x2 + [0] * (maxlen-len(x2)) if len(x2) < maxlen else x2
+    text1 = explanation1 if example1 == "无" else explanation1 + example1
+    text2 = explanation2 if example2 == "无" else explanation2 + example2
+    text1 = text1[: maxlen // 2]
+    text2 = text2[: maxlen // 2]
+    x1, x2 = tokenizer.encode(first=text1, second=text2)
 
-# 模型预测并输出预测结果
-predicted = model.predict([[X1], [X2]])
-print(predicted)
+    X1 = x1 + [0] * (maxlen - len(x1)) if len(x1) < maxlen else x1
+    X2 = x2 + [0] * (maxlen - len(x2)) if len(x2) < maxlen else x2
+
+    # 模型预测并输出预测结果
+    predicted = model.predict([[X1], [X2]])
+    predicted = predicted.tolist()[0]
+    print(predicted)
+
+    if predicted[0] > predicted[1] and predicted[0] > predicted[2]:
+        print("两成语无逻辑关系")
+    elif predicted[1] > predicted[0] and predicted[1] > predicted[2]:
+        print("两成语具有并列关系")
+    else:
+        print("两成语具有转折关系")
+
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
